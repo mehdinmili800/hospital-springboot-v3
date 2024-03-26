886b1844c028e6c77211cda9613e6504b40f4d2a
mehdi800wa@gmail.com <mehdi800wa@gmail.com>
2024-03-25 18:10:37 -0700

    update 3

--- src/main/java/com/example/management/config/WebConfig.java
+++ src/main/java/com/example/management/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/auth")
                 .allowedOrigins("https://schoolhanae.site")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*") // Specify allowed headers
